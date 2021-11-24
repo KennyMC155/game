188599c26ad94e37ee81808afc57593d7d6c66a4
Anton <anton@macs-MacBook-Air.local>
2021-11-24 21:32:37 +0200

    Second try to make functions work corretcly in my game, but lose ability to move rockets

--- Main.py
+++ Main.py
@@ -1,8 +1,12 @@
 from functions import *
 
+ball = create_ball()
+left_rocket = creeate_roket("blue", -350)
+right_rocket = creeate_roket("red", 350)
+pen = create_score_board()
 
 while True:
-    create_game_win()
+    create_game_win(left_rocket, right_rocket)
     # ball movements
     ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
@@ -16,16 +20,16 @@ while True:
         ball.dy *= -1
 
     if ball.xcor() > 390:
-        ball_return()
+        ball_return(ball)
         score_a += 1
         pen.clear()
         pen.write(f"{player_a}: {score_a} || {player_b}: {score_b}", align="center", font=("Verdana", 22, "normal"))
 
     if ball.xcor() < -390:
-        ball_return()
+        ball_return(ball)
         score_b += 1
         pen.clear()
         pen.write(f"{player_a}: {score_a} || {player_b}: {score_b}", align="center", font=("Verdana", 22, "normal"))
 
-    rocket_push()
-    get_win(score_a, score_b)
+    rocket_push(ball, left_rocket, right_rocket)
+    get_win(pen, score_a, score_b, ball, left_rocket, right_rocket)
--- functions.py
+++ functions.py
@@ -8,17 +8,17 @@ player_a = "Left Player"
 player_b = "Right Player"
 
 
-def create_game_win():
+def create_game_win(left_rocket, right_rocket):
     win = turtle.Screen()
     win.title("Game")
     win.bgcolor("black")
     win.setup(width=800, height=600)
     win.tracer(0)
     win.listen()
-    win.onkeypress(rocket_left_up, "w")
-    win.onkeypress(rocket_left_down, "s")
-    win.onkeypress(rocket_right_up, "Up")
-    win.onkeypress(rocket_right_down, "Down")
+    win.onkeypress(rocket_up(left_rocket), "w")
+    win.onkeypress(rocket_down(left_rocket), "s")
+    win.onkeypress(rocket_up(right_rocket), "Up")
+    win.onkeypress(rocket_down(right_rocket), "Down")
     win.update()
     return win
 
@@ -34,10 +34,6 @@ def creeate_roket(color, postion_cord):
     return rocket
 
 
-left_rocket = creeate_roket("blue", -350)
-right_rocket = creeate_roket("red", 350)
-
-
 # create ball
 def create_ball():
     ball = turtle.Turtle()
@@ -46,12 +42,9 @@ def create_ball():
     ball.color("yellow")
     ball.penup()
     ball.goto(0, 0)
-    ball.dx = 4
-    ball.dy = 4
+    ball.dx = 10
+    ball.dy = 0
     return ball
-
-
-ball = create_ball()
 
 
 # create_score_board
@@ -67,12 +60,7 @@ def create_score_board():
     return pen
 
 
-pen = create_score_board()
-
-
 # rockets movement
-
-
 def rocket_left_up():
     y = left_rocket.ycor()
     if y < 250:
@@ -80,30 +68,14 @@ def rocket_left_up():
         left_rocket.sety(y)
 
 
-def rocket_left_down():
-    y = left_rocket.ycor()
+def rocket_down(rocket):
+    y = rocket.ycor()
     if y > -250:
         y -= 20
-        left_rocket.sety(y)
+        rocket.sety(y)
 
 
-def rocket_right_up():
-    x = right_rocket.ycor()
-    if x < 250:
-        x += 20
-        right_rocket.sety(x)
-
-
-def rocket_right_down():
-    x = right_rocket.ycor()
-    if x > - 250:
-        x -= 20
-        right_rocket.sety(x)
-
-
-# ball movement
-
-def clean_win():
+def clean_win(pen, ball, left_rocket, right_rocket):
     pen.clear()
     ball.clear()
     left_rocket.clear()
@@ -111,26 +83,27 @@ def clean_win():
 
 
 # ball return
-def ball_return():
+def ball_return(ball):
     ball.goto(0, 0)
     ball.dx *= -1
 
 
-def get_win(score_a, score_b):
+def get_win(pen, score_a, score_b, ball, left_rocket, right_rocket):
     if score_a > score:
-        clean_win()
+        clean_win(pen, ball, left_rocket, right_rocket)
         pen.write(f"{player_a} Win", align="center", font=("Verdana", 22, "normal"))
         time.sleep(3)
         turtle.bye()
     elif score_b > score:
-        clean_win()
+        clean_win(pen, ball, left_rocket, right_rocket)
         pen.write(f"{player_b} Win", align="center", font=("Verdana", 22, "normal"))
         time.sleep(3)
         turtle.bye()
 
 
-def rocket_push():
-    if ball.xcor() > 340 and ball.ycor() < right_rocket.ycor() + 50 and ball.ycor() > right_rocket.ycor() - 50:
+# ball movement
+def rocket_push(ball, left_rocket, right_rocket):
+    if ball.xcor() > 340 and right_rocket.ycor() + 50 > ball.ycor() > right_rocket.ycor() - 50:
         ball.dx *= -1
-    if ball.xcor() < -340 and ball.ycor() < left_rocket.ycor() + 50 and ball.ycor() > left_rocket.ycor() - 50:
+    if ball.xcor() < -340 and left_rocket.ycor() + 50 > ball.ycor() > left_rocket.ycor() - 50:
         ball.dx *= -1
